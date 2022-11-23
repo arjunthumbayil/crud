d96925a6433f03dacec7cd5908f5b49be4510aee
Arjun Thumbayil <arjunthumbayil@gmail.com>
2022-11-23 20:58:20 +0530

    useRef hook with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,23 @@ import { Todo } from './model'
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
-  console.log(todo);
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }])
+      setTodo("")
+    }
+  };
+
+  console.log(todos);
   return (
     <div className="App">
       <span className='heading'>Tasko</span>
-      <InputField todo={todo} setTodo={setTodo} />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {/* <TodoList /> */}
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css"
 
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
-    return <form className="input">
-        <input className="input__box" type="input" placeholder="Type out the task that is bothering you atm! "
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+
+    return <form className="input" onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur()
+    }}>
+        <input ref={inputRef} className="input__box" type="input" placeholder="Type out the task that is bothering you atm! "
             value={todo} onChange={(e) => setTodo(e.target.value)}
         />
         <button className="input_submit" type="submit">Push</button>
